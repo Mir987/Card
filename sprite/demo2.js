e2ba3d0a4ba57f1fa20ed362b48bf02d65c36da5
zamfirm <m.zamfir@plan-net.com>
2022-06-06 16:29:52 +0300

    header fix + animation rework

--- sprite/demo2.js
+++ sprite/demo2.js
@@ -3,8 +3,8 @@ import './style.css'
 let cardglare = document.querySelector('#card_glare');
 let shadow = document.querySelector('#shadow');
 let glare = document.querySelector('#glare');
-let Dspeed = 8;      // speed of the animation on scroll (low value = hi speed) Ex: 8 = desktop, 5.5 = mobile
-let Mspeed = 5.5;
+let box = document.querySelector('#box');
+let cardSpeed = 4;      // speed of the card rotation on scroll (low value = hi speed)
 
 window.onload = resetScroll;
 
@@ -15,16 +15,20 @@ window.addEventListener("resize", event => {
 window.addEventListener("scroll", event => {
     let i;
     let scrollVal = parseInt(window.scrollY);
-    if(window.innerWidth >= 576){
-        i = scrollVal / Dspeed;
-    }else{
-        i = scrollVal / Mspeed;
-    }
+    i = scrollVal / cardSpeed;
 
     if(i <= 29){
         cardglare.style = "transform: rotateX(" + (i * 2.5) + "deg)";
-        shadow.style = "transform: translateX(-50%) rotateX(" + (i * 2.5) + "deg); width:" + (50 + (i * 0.833)) + "%; bottom: " + (15 + (i * 0.233)) + "%; opacity:" + (0.8 - (i * 0.02));
+        // shadow.style = "transform: translateX(-50%) rotateX(" + (i * 2.5) + "deg); width:" + (50 + (i * 0.833)) + "%; bottom: " + (15 + (i * 0.233)) + "%; opacity:" + (0.8 - (i * 0.02));
+        shadow.style = "transform: translateX(-50%) rotateX(" + (i * 2.5) + "deg); width:" + (50 + (i * 0.833)) + "%; bottom: " + (15 + (Math.pow(i/5, 2))) + "%; opacity:" + (0.8 - (i * 0.02));
+        console.log((Math.pow(i/2, 2)));
         glare.style = "transform: translate(-50%," + (-100 + (i * 5)) + "%); width:" + (30 + i) + "%; height:" + 300 + "px";
+        // box.style = "transform: translateY(" + (i * -7) + "px)";
+        if(window.innerWidth < 576){
+            box.style = "transform: translateY(" + (-(Math.pow(i/3, 2))) + "px)";
+        }else{
+            box.style = "transform: translateY(" + (-(Math.pow(i/2, 2))) + "px)";
+        }
     }
 });
 
